9b418fe02698a4c52e61ab780bd1b6bd958b4982
kbordovskii <kbordovskii@isgneuro.com>
2020-08-07 22:55:12 +0300

    fixed all test commands (but not test results)

--- src/test/scala/ot/dispatcher/plugins/small/SmallFitGradientBoostingTest.scala
+++ src/test/scala/ot/dispatcher/plugins/small/SmallFitGradientBoostingTest.scala
@@ -26,30 +26,30 @@ class SmallFitGradientBoostingTest extends CommandTest {
 
 
   test("Test 1. Command: | apply random_forest") {
-    val queryFit = SimpleQuery(""" random_forest class from a b c into rf_test""")
+    val queryFit = SimpleQuery(""" gradient_boosting class from a b c into gb_test""")
     val commandFit = new SmallFit(queryFit, utils)
 
-    val queryApply = SimpleQuery("""rf_test""")
+    val queryApply = SimpleQuery("""gb_test""")
     val commandApply = new SmallApply(queryApply, utils)
 
     val actual = execute(commandFit, commandApply)
     val expected =
       """[
-        |{"a":1,"class":"1a","b":2,"c":2,"target":1,"_time":1,"raw_prediction":{"type":1,"values":[0.0,9.0]},"probability_prediction":{"type":1,"values":[0.0,1.0]},"rf_test_prediction":"1a"},
-        |{"a":2,"class":"2b","b":3,"c":2,"target":2,"_time":1,"raw_prediction":{"type":1,"values":[6.0,3.0]},"probability_prediction":{"type":1,"values":[0.6666666666666666,0.3333333333333333]},"rf_test_prediction":"2b"}
+        |{"a":1,"class":"1a","b":2,"c":2,"target":1,"_time":1,"raw_prediction":{"type":1,"values":[0.0,9.0]},"probability_prediction":{"type":1,"values":[0.0,1.0]},"gb_test_prediction":"1a"},
+        |{"a":2,"class":"2b","b":3,"c":2,"target":2,"_time":1,"raw_prediction":{"type":1,"values":[6.0,3.0]},"probability_prediction":{"type":1,"values":[0.6666666666666666,0.3333333333333333]},"gb_test_prediction":"2b"}
         |]""".stripMargin
 
     assert(jsonCompare(actual, expected), f"Result : $actual\n---\nExpected : $expected")
   }
 
-  test("Test 2. Command: | fit random_forest") {
-    val query = SimpleQuery(""" random_forest max=2 class from a b c into rf_test""")
+  test("Test 2. Command: | fit gradient_boosting") {
+    val query = SimpleQuery(""" gradient_boosting max=2 class from a b c into gb_test""")
     val command = new SmallFit(query, utils)
     val actual = execute(command)
     val expected =
       """[
-        |{"a":1,"class":"1a","b":2,"c":2,"target":1,"_time":1,"raw_prediction":{"type":1,"values":[0.0,9.0]},"probability_prediction":{"type":1,"values":[0.0,1.0]},"rf_test_prediction":"1a"},
-        |{"a":2,"class":"2b","b":3,"c":2,"target":2,"_time":1,"raw_prediction":{"type":1,"values":[6.0,3.0]},"probability_prediction":{"type":1,"values":[0.6666666666666666,0.3333333333333333]},"rf_test_prediction":"2b"}
+        |{"a":1,"class":"1a","b":2,"c":2,"target":1,"_time":1,"raw_prediction":{"type":1,"values":[0.0,9.0]},"probability_prediction":{"type":1,"values":[0.0,1.0]},"gb_test_prediction":"1a"},
+        |{"a":2,"class":"2b","b":3,"c":2,"target":2,"_time":1,"raw_prediction":{"type":1,"values":[6.0,3.0]},"probability_prediction":{"type":1,"values":[0.6666666666666666,0.3333333333333333]},"gb_test_prediction":"2b"}
         |]""".stripMargin
 
     assert(jsonCompare(actual, expected), f"Result : $actual\n---\nExpected : $expected")
